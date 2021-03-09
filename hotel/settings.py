59e8a21eaf6e3392f5d7e08105135cff66801e9a
wmukasa <wmukasa@gmail.com>
2021-03-09 22:47:37 +0300

    Update settings.py

--- hotel/settings.py
+++ hotel/settings.py
@@ -69,7 +69,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
